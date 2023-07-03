1c97da0c58c469be918df6322f71a84bdec67881
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-07-02 20:20:34 -0700

    hooks, renderprops

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { TextField } from './TextField';
+// import { TextField } from './TextField';
+import { Counter } from './Counter';
 
 const App: React.FC = () => {
   return (
     <div>
-      <TextField text='Hello' person={{ firstName: '', lastName: '' }}
+      {/* <TextField text='Hello' person={{ firstName: '', lastName: '' }}
         handleChange={e => {
           e.stopPropagation();
       }}
-      />
+      /> */}
+
+      <Counter>
+        {({count, setCount}) => (<div>{count}<button onClick={()=> setCount(count+1)}>+</button></div>)}
+      </Counter>
     </div>
   )
 }
